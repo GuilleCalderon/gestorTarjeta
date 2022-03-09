bed47c1137675ff9a2d5b5a39c3f9c923573c6ce
GuilleCalderon <guillermo.calde46@gmail.com>
2022-03-09 16:56:50 -0300

    con validaciones andando

--- src/components/FormularioTarjeta.js
+++ src/components/FormularioTarjeta.js
@@ -1,113 +1,132 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const initialForm = {
-cardType: "credito",
-number: "",
-name:"",
-exp:"",
-segurity:""
-}
+  cardType: "credito",
+  number: "",
+  name: "",
+  exp: "",
+  segurity: "",
+};
 
+const validateForm = (form) => {
+  let errors = {};
 
-const FormularioTarjeta = ({addToCardList,closeModal}) => {
+  if (!form.name?.trim()) {
+    errors.name = "El NOMBRE que figura en la tarjeta es requerido";
+  }
 
-  const [form, setForm] = useState(initialForm)
+  if (!form.number?.trim()) {
+    errors.number = "El NUMERO de la tarjeta es requerido";
+  }
 
-  const [errors, setErrors] = useState({})
+  if (!form.exp?.trim()) {
+    errors.exp = "La fecha de EXPIRACION de la tarjeta es requerida";
+  }
 
-  const handleRatio= (e)=>{
+  if (!form.segurity?.trim()) {
+    errors.segurity = "El CODIGO de seguridad de la tarjeta es requerido";
+  }
+
+  return errors;
+};
+
+const FormularioTarjeta = ({ addToCardList, closeModal }) => {
+  const [form, setForm] = useState(initialForm);
+
+  const [errors, setErrors] = useState({});
+
+  const handleRatio = (e) => {
     setForm({
       ...form,
-      cardType: e.target.id
-      
-    })
-    
-  }
+      cardType: e.target.id,
+    });
+  };
 
-  const handleChange = (e)=>{
-
+  const handleChange = (e) => {
     setForm({
       ...form,
-      [e.target.name] : e.target.value,
-      
-    })
-    
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  const handleBlur = (e)=>{}
+  const handleBlur = (e) => {
+    handleChange(e);
 
-  const handleSubmit = (e)=>{
-    e.preventDefault()
+    setErrors(validateForm(form));
+  };
 
-    addToCardList(form)
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    closeModal()
+    addToCardList(form);
 
-    setForm(initialForm)    
+    closeModal();
 
-  }
+    setForm(initialForm);
+  };
 
   return (
-
-    <form className='formulario' onSubmit={handleSubmit}>
-      <p>Datos de su nueva tarjeta a cargar:</p>
-      
-      <input 
-      onClick={handleRatio} 
-      type="radio" 
-      name="elegi" 
-      id='credito' 
-      defaultChecked={true}
-      />Credito
-      <input 
-      onClick={handleRatio} 
-      type="radio" 
-      name="elegi" 
-      id="debito" 
-      />Debito<br /><br />
-
-      <input 
-      type="text" 
-      placeholder='Nombre que figura en la tarjeta' 
-      name='name' 
-      value={form.name} 
-      onChange={handleChange}
-      required
-      /><br /><br />
-
-      <input 
-      type="number" 
-      placeholder='Numero de la tarjeta' 
-      name='number' 
-      value={form.number} 
-      onChange={handleChange}
-      required
-      /><br /><br />
-
-      <input 
-      type="text" 
-      placeholder='Fecha de expiracion' 
-      name='exp' 
-      value={form.exp} 
-      onChange={handleChange}
-      required
-      /><br /><br />
-
-      <input 
-      type="number" 
-      placeholder='Codigo de seguridad' 
-      name='segurity' 
-      value={form.segurity} 
-      onChange={handleChange}
-      required
-      /><br /><br />
-
-      <button type='submit'>Crear</button>
-
-
+    <form className="formulario" onSubmit={handleSubmit}>
+      <p className="titulo-form">Datos de su nueva tarjeta a cargar:</p>
+      <input
+        onClick={handleRatio}
+        type="radio"
+        name="elegi"
+        id="credito"
+        defaultChecked={true}
+      />
+      Credito
+      <input onClick={handleRatio} type="radio" name="elegi" id="debito" />
+      Debito
+      <br />
+      <br />
+      <input
+        onBlur={handleBlur}
+        type="text"
+        placeholder="Nombre que figura en la tarjeta"
+        name="name"
+        value={form.name}
+        onChange={handleChange}
+        required
+      />
+      {errors.name && <p className="validation">{errors.name}</p>}
+      <input
+        onBlur={handleBlur}
+        type="number"
+        placeholder="Numero de la tarjeta"
+        name="number"
+        value={form.number}
+        onChange={handleChange}
+        required
+      />
+      {errors.number && <p className="validation">{errors.number}</p>}
+      <input
+        onBlur={handleBlur}
+        type="text"
+        placeholder="Fecha de expiracion"
+        name="exp"
+        value={form.exp}
+        onChange={handleChange}
+        required
+      />
+      {errors.exp && <p className="validation">{errors.exp}</p>}
+      <input
+        onBlur={handleBlur}
+        type="number"
+        placeholder="Codigo de seguridad"
+        name="segurity"
+        value={form.segurity}
+        onChange={handleChange}
+        required
+      />
+      {errors.segurity && <p className="validation">{errors.segurity}</p>}
+      <br />
+      <br />
+      <button className="btn-submit" type="submit">
+        Agregar Tarjeta
+      </button>
     </form>
-    
-  )
-}
+  );
+};
 
-export default FormularioTarjeta
+export default FormularioTarjeta;
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import "./Modal.css"
+import React from "react";
+import "./Modal.css";
 
-const Modal = ({children, isOpen, closeModal}) => {
-
-  const handleModalContainerClick= e => e.stopPropagation()
+const Modal = ({ children, isOpen, closeModal }) => {
+  const handleModalContainerClick = (e) => e.stopPropagation();
 
   return (
-    <article className= {`modal ${isOpen && "is-open"}`} onClick={closeModal}>
+    <article className={`modal ${isOpen && "is-open"}`}>
       <div className="modal-container" onClick={handleModalContainerClick}>
-        <button className="modal-close" onClick={closeModal}>X</button>
+        <button className="modal-close" onClick={closeModal}>
+          X
+        </button>
         {children}
       </div>
     </article>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
